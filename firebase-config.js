3dc6fb038e49f8113183b51ab94d45318610e0a7
yuimellkub <alifnarak29@gmail.com>
2026-09-10 17:03:49 +0700

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,12 +1,1 @@
-// Yuimellkub Firebase configuration
-window.YUIMELLKUB_FIREBASE_CONFIG = {
-  apiKey: "AIzaSyB2_JvGY-ttHw-32yYZ03FMDsW8R5S6BSE",
-  authDomain: "yuimellkub-topup.firebaseapp.com",
-  projectId: "yuimellkub-topup",
-  storageBucket: "yuimellkub-topup.firebasestorage.app",
-  messagingSenderId: "858747570015",
-  appId: "1:858747570015:web:314732a955dec360df18ea",
-  measurementId: "G-VBPC096X50"
-};
-
-window.YUIMELLKUB_ADMIN_UID = "NK2Um4UJRnca2VLa8FPJObq6ZFm2";
+window.YUIMELLKUB_ADMIN_UID = "xu43sk3PvOddYjmhyJPBXcCdmUn2";
