e4ec3106393036812fe7eb2dcf70181d8620fa32
yuimellkub <alifnarak29@gmail.com>
2026-09-12 16:30:00 +0700

    Configure EmailJS order notifications

--- emailjs-config.js
+++ emailjs-config.js
@@ -1,7 +1,7 @@
 // Yuimellkub order email notification settings
 // These three values are public EmailJS identifiers, not passwords.
 window.YUIMELLKUB_EMAILJS = {
-  publicKey: '',
-  serviceId: '',
-  templateId: ''
+  publicKey: '07m6sC6efee4ZTuPZ',
+  serviceId: 'service_6i0b9bq',
+  templateId: 'template_09h51ng'
 };
