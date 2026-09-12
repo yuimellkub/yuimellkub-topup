804fe3a97829b7a470c35098b03e6c91a307092e
yuimellkub <alifnarak29@gmail.com>
2026-09-12 16:11:14 +0700

    Add EmailJS notification config placeholder

---
+++ emailjs-config.js
@@ -0,0 +1,7 @@
+// Yuimellkub order email notification settings
+// These three values are public EmailJS identifiers, not passwords.
+window.YUIMELLKUB_EMAILJS = {
+  publicKey: '',
+  serviceId: '',
+  templateId: ''
+};
